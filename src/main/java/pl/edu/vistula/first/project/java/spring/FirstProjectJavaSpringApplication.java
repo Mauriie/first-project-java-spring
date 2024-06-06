5e12d68c022bfc836e43a35de5af0622698446dd
Mauriie <mmariko1@stu.vistula.edu.pl>
2024-06-06 14:00:40 +0200

    Initial commit

---
+++ src/main/java/pl/edu/vistula/first/project/java/spring/FirstProjectJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first.project.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
+	}
+
+}
